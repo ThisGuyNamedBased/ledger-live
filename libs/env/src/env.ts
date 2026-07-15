4aa8b0a9d921802386a70f0a096ea7974ba372f9
Louis Aussedat <louis.aussedat@ledger.fr>
2026-07-15 07:58:46 +0200

    feat(live-dmk-desktop): add mock server transport toggle

--- libs/env/src/env.ts
+++ libs/env/src/env.ts
@@ -162,6 +162,7 @@ export interface EnvTypes {
   EXPERIMENTAL_USB: boolean;
   MOCK_APP_UPDATE: boolean;
   FORCE_PROVIDER: number;
+  MOCK_SERVER_TRANSPORT: boolean;
   MOCK: string;
   MOCK_OS_VERSION: string;
   MOCK_NO_BYPASS: boolean;
